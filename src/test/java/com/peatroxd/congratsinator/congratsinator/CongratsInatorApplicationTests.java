6cb67ba3eacb2078dc607b2780f139ce397abacf
PeatroXD <PeatroXD@yandex.ru>
2026-01-14 15:23:58 +0300

    -Initial Commit

---
+++ src/test/java/com/peatroxd/congratsinator/congratsinator/CongratsInatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.peatroxd.congratsinator.congratsinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongratsInatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
