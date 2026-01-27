6cd7a6f4aacb3e314fb401edf813ef734a5e98c4
PeatroXD <PeatroXD@yandex.ru>
2026-01-27 13:41:30 +0300

    Enhance Person entity with equals and hashCode methods, and update annotations for better clarity

--- src/main/java/com/peatroxd/congratsinator/model/Person.java
+++ src/main/java/com/peatroxd/congratsinator/model/Person.java
@@ -1,9 +1,13 @@
 package com.peatroxd.congratsinator.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
@@ -11,7 +15,7 @@ import java.util.UUID;
 
 @Entity
 @Table(name = "person")
-@Data
+
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -21,9 +25,25 @@ public class Person {
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
 
+    @Column(nullable = false)
     private String name;
 
     private LocalDate birthday;
 
     private String photoKey;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        return id != null && id.equals(person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
 }
