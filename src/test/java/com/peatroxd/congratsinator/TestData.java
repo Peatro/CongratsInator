50a4036926f64f915e194141829aa3063a18cd47
PeatroXD <PeatroXD@yandex.ru>
2026-01-27 16:22:41 +0300

    Add unit tests for SmtpEmailSenderServiceImpl to validate email sending functionality

--- src/test/java/com/peatroxd/congratsinator/TestData.java
+++ src/test/java/com/peatroxd/congratsinator/TestData.java
@@ -14,6 +14,8 @@ public class TestData {
     public static final LocalDate BIRTHDAY_DATE = LocalDate.of(1990, 1, 1);
     public static final String PHOTO_KEY = "photo_key_123";
     public static final String PERSON_NAME = "John Doe";
+    public static final String NOTIFICATION_EMAIL_SUBJECT = "Upcoming Birthdays";
+    public static final String NOTIFICATION_EMAIL_BODY = "Hello there";
 
     public static Person createPerson() {
         return johnDoe(generateRandomUUID(), BIRTHDAY_DATE);
---
+++ src/test/java/com/peatroxd/congratsinator/service/impl/SmtpEmailSenderServiceImplTest.java
@@ -0,0 +1,59 @@
+package com.peatroxd.congratsinator.service.impl;
+
+import com.peatroxd.congratsinator.service.EmailSenderService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.util.ReflectionTestUtils;
+
+import java.util.List;
+
+import static com.peatroxd.congratsinator.TestData.NOTIFICATION_EMAIL_BODY;
+import static com.peatroxd.congratsinator.TestData.NOTIFICATION_EMAIL_SUBJECT;
+import static com.peatroxd.congratsinator.TestData.createListOfEmails;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
+@ExtendWith(MockitoExtension.class)
+class SmtpEmailSenderServiceImplTest {
+
+    private static final String FROM = "peatroxd@gmail.com";
+    public static final String MAIL_SENDER_FROM_FIELD = "from";
+
+    @Mock
+    private JavaMailSender mailSender;
+
+    private EmailSenderService emailSender;
+
+    @BeforeEach
+    void setUp() {
+        emailSender = new SmtpEmailSenderServiceImpl(mailSender);
+        ReflectionTestUtils.setField(emailSender, MAIL_SENDER_FROM_FIELD, FROM);
+    }
+
+    @Test
+    void send_buildsMessageAndDelegatesToJavaMailSender() {
+        List<String> to = createListOfEmails();
+        String subject = NOTIFICATION_EMAIL_SUBJECT;
+        String body = NOTIFICATION_EMAIL_BODY;
+
+        emailSender.send(to, subject, body);
+
+        ArgumentCaptor<SimpleMailMessage> captor = ArgumentCaptor.forClass(SimpleMailMessage.class);
+        verify(mailSender).send(captor.capture());
+        verifyNoMoreInteractions(mailSender);
+
+        SimpleMailMessage msg = captor.getValue();
+
+        assertThat(msg.getFrom()).isEqualTo(FROM);
+        assertThat(msg.getTo()).containsExactly("a@ex.com", "b@ex.com");
+        assertThat(msg.getSubject()).isEqualTo(subject);
+        assertThat(msg.getText()).isEqualTo(body);
+    }
+}
